fbce2282d406cfae1ad27160650d6d3ccdacc691
Aeron Potkek <56285457+potkek01@users.noreply.github.com>
2020-03-10 16:27:41 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1583868312,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/9patch.png",
+		"images/distancedisplay.png",
+		"images/gay-sheet0.png",
+		"media/gta thing.ogg",
+		"media/jooooooooooj.ogg",
+		"media/oj.ogg",
+		"media/oof.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
